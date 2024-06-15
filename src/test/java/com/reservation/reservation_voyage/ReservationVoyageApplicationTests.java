56fdc455705906a008c31de80d8df3007ea24c82
TchassiDaniel <tchassidaniel@gmail.com>
2024-06-15 13:12:50 +0100

    hello

---
+++ src/test/java/com/reservation/reservation_voyage/ReservationVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.reservation_voyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
